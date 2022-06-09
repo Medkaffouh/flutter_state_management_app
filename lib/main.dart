52bbcce889e2908f8a5bc3afad6bc24145a0c19f
Medkaffouh <kaffouhmohamed@gmail.com>
2022-06-09 15:34:59 +0100

    state provider, how to consume the state in the widget with Consumer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_management_app/pages/counter.provider.page.dart';
 import 'package:flutter_state_management_app/pages/counter.stateful.page.dart';
+import 'package:flutter_state_management_app/provider/counter.state.dart';
+import 'package:provider/provider.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,14 +10,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.teal
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context)=>CounterState())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.teal
+        ),
+        routes: {
+          "/":(context) => CounterProviderPage()
+        },
       ),
-      routes: {
-        "/":(context) => CounterPage()
-      },
     );
   }
 }
---
+++ lib/pages/counter.provider.page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_management_app/provider/counter.state.dart';
+import 'package:provider/provider.dart';
+
+class CounterProviderPage extends StatelessWidget {
+  const CounterProviderPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    CounterState counterState = Provider.of<CounterState>(context,listen: false);
+    print("Build .......");
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter Provider'),),
+      body: Consumer<CounterState>(
+        builder: (context,counterState,child){
+          print("Text ....");
+          return Center(
+            child: Text('Counter Value => ${counterState.counter}',style: Theme.of(context).textTheme.headline4,),
+          );
+        },
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: (){
+                //Provider.of<CounterState>(context,listen: false).decrement();
+                counterState.decrement();
+              }
+          ),
+          SizedBox(width: 4,),
+          FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: (){
+                counterState.increment();
+              }
+          )
+        ],
+      ),
+    );
+  }
+}
